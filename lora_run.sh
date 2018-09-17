c9b788b5ed9d4992e193e102c9959bbeaefb3d10
Dee <fantp6@gmail.com>
2018-09-17 17:56:16 +0800

    change freq

--- lora_run.sh
+++ lora_run.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-freq=("923.2" "923.4" "923.6" "923.8" "924.0" "924.2" "924.4" "924.6")
-send_freq="${freq[$RANDOM%8]}"
+#freq=("923.2" "923.4" "923.6" "923.8" "924.0" "924.2" "924.4" "924.6")
+freq=("923.2" "923.4")
+send_freq="${freq[$RANDOM%2]}"
 
 car=$1
 count=$2
-
-printf "%s\n" "${car}"
-printf "%s\n" "${count}"
 
 if [ $count -eq 1 ]; then
     python3 join_otaa_retry.py
