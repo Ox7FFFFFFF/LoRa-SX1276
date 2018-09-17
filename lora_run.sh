ac84225f18a20d8f8e036f686dab415f14abb423
Dee <fantp6@gmail.com>
2018-09-17 16:16:36 +0800

    upload script

---
+++ lora_run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+freq=("923.2" "923.4" "923.6" "923.8" "924.0" "924.2" "924.4" "924.6")
+send_freq="${freq[$RANDOM%8]}"
+
+car=$1
+count=$2
+
+printf "%s\n" "${car}"
+printf "%s\n" "${count}"
+
+if [ $count -eq 15 ]; then
+    python3 join_otaa_retry.py
+    sleep 10
+fi
+
+python3 send_otaa_retry.py --freq ${send_freq} --parking ${car}
